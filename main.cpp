de8ec8b7acb7e0a38c7fed4ac23f211e7a8960c0
Ivan Sharapenkov <ivansharapenkov@gmail.com>
2019-04-13 22:53:12 +0300

    Add ultralight support

--- main.cpp
+++ main.cpp
@@ -1,12 +1,24 @@
 #include <iostream>
 #include <string>
+#include <Ultralight/Ultralight.h>
+#include <AppCore/AppCore.h>
 #include "runtime/Game.h"
 #include "field/Field.h"
 #include "player/Player.h"
 
 using namespace std;
+using namespace ultralight;
 
 int main() {
+
+    auto app = App::Create();
+
+    auto window = Window::Create(app->main_monitor(), 1024, 768, false,
+                             kWindowFlags_Resizable | kWindowFlags_Titled | kWindowFlags_Maximizable);
+    window->SetTitle("Xgame");
+    app->set_window(window.get());
+
+    app->Run();
 
     shared_ptr<Player> p1 = make_shared<Player>();
     shared_ptr<Player> p2 = make_shared<Player>();
